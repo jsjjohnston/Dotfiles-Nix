459c2791b2f11849426a8cb8cf635132efedee92
Jay Johnston <jsjjohnston@gmail.com>
2024-07-04 17:14:40 +1000

    init

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "NixOS Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-colors.url = "github:misterio77/nix-colors";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: 
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      
+      config = {
+        allowUnfree = true;
+      };
+    };
+   in
+   {
+
+    nixosConfigurations = {
+      		nixos = nixpkgs.lib.nixosSystem {
+			specialArgs = { inherit inputs system; };
+                        modules = [
+                          ./configuration.nix
+                        ];
+		};
+    	};
+
+    };
+}
