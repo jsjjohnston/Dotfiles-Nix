3fa84fc77f0b2eae7b3d155f62002f61a6780266
Jay Johnston <jsjjohnston@gmail.com>
2024-07-16 20:32:37 +1000

    new home.nix

--- home.nix
+++ home.nix
@@ -1,18 +1,10 @@
+{ config, pkgs, ... }:
+
 {
-  config,
-  pkgs,
-  #inputs,
-  ...
-}: {
-  imports = [
-    inputs.nix-colors.homeManagerModules.default
-  ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "jay";
   home.homeDirectory = "/home/jay";
-
-  #colorScheme = inputs.nix-colors.colorSchemes.dracula;
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -21,31 +13,7 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
-
-  #programs.neovim = let
-  #  toLua = str: "lua << EOF\n${str}\n\EOF\n";
-  #  toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
-  #in {
-  #  enable = true;
-
-  #  viAlias = true;
-  #  vimAlias = true;
-  #  vimdiffAlias = true;
-
-  #  defaultEditor = true;
-  #  plugins = with pkgs.vimPlugins; [
-  #    vim-nix
-
-  #    {
-  #      plugin = cyberdream-nvim;
-  #      config = toLuaFile ./nvim/lua/kickstart/plugins/colorscheme.lua;
-  #    }
-  #  ];
-    #extraLuaConfig = ''
-    #  ${builtins.readFile ./nvim/init.lua}
-    #'';
-  #};
+  home.stateVersion = "23.11"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -63,7 +31,7 @@
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
-    #(pkgs.writeShellScriptBin "my-hello" ''
+    # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
   ];
@@ -74,7 +42,8 @@
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
-    # ".config/nvim".source = "./nvim";
+    # ".screenrc".source = dotfiles/screenrc;
+    "./.config/hypr/hyperland.conf".source = config/hypr/hyperland.conf;
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
